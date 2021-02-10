a987697dc07a992576dd3bd9f8a8306961595f50
Yorams <yscheer@gmail.com>
2021-02-10 22:52:56 +0100

    Added auto refresh to anwers page and mark them green if answered

--- public/javascripts/score_public.js
+++ public/javascripts/score_public.js
@@ -8,7 +8,7 @@ Handlebars.registerHelper('debug', function (data) {
 
 Handlebars.registerHelper('getRoundScore', function (data, key) {
     var returnData = 0;
-    if (typeof (data[key]) !== "undefined"){
+    if (typeof (data[key]) !== "undefined") {
         returnData = Math.round((data[key] + Number.EPSILON) * 100) / 100
     }
 
@@ -18,28 +18,28 @@ Handlebars.registerHelper('getRoundScore', function (data, key) {
 // Formateer antwoorden voor de popover
 Handlebars.registerHelper('formatAnswer', function (data, type) {
     var returnData = "";
-    if (type == "one" || type == "multi" || type == "open-numeric" || type == "open-text"){
+    if (type == "one" || type == "multi" || type == "open-numeric" || type == "open-text") {
         returnData = data.default
-    } else if(type == "music"){
+    } else if (type == "music") {
         returnData = `<b>Artiest:</b> ${data.artist}<br> <b>Titel:</b> ${data.title}`
-    } else if (type == "music-locatie"){
+    } else if (type == "music-locatie") {
         returnData = `<b>Artiest:</b> ${data.artist}<br> <b>Titel:</b> ${data.title}<br> <b>Locatie:</b> ${data.locatie}`
     }
     return returnData
 })
 
 Handlebars.registerHelper('showScore', function (data) {
-    if(typeof(data) !== "undefined"){
+    if (typeof (data) !== "undefined") {
         return data;
-    }else{
+    } else {
         return 0
     }
 })
 
 Handlebars.registerHelper("inc", function (value, add) {
-    if(typeof(add) !== "undefined"){
+    if (typeof (add) !== "undefined") {
         return parseInt(value) + 1 + add;
-    }else{
+    } else {
         return parseInt(value) + 1;
     }
 });
@@ -50,7 +50,7 @@ Handlebars.registerHelper('round', function (score) {
 
 Handlebars.registerHelper('topStyle', function (place) {
     var returnData = "";
-    if (place == 0){
+    if (place == 0) {
         returnData = "topFirst"
     } else if (place == 1) {
         returnData = "topSecond"
@@ -67,40 +67,83 @@ var tooltipSettings = {
     selector: '.scoreNumber',
 }
 
+var refreshTimer;
+
+
 jQuery(function () {
-    $(".scoreMain").on("mouseover", ".scoreNumber", function(){
+    $(".scoreMain").on("mouseover", ".scoreNumber", function () {
         $(".scoreNumber").popover('hide')
         $(this).popover('show')
     })
+
+    // Toggle auto refresh
+    $(".btnAutoRefresh").on("click", function () {
+        // Toggle icon and class
+        $(this).find("button").toggleClass("btn-success")
+        $(this).find("i").toggleClass("mdi-power-on")
+
+        $(this).find("button").toggleClass("btn-danger")
+        $(this).find("i").toggleClass("mdi-power-off")
+
+        // Start of cancel timer
+        if ($(this).find("i").hasClass("mdi-power-on")) {
+            loadScore("overview");
+            startRefreshTimer()
+        } else {
+            clearInterval(refreshTimer);
+        }
+    })
 })
 
-function loadScore(view){
-    $.post("getScore", function(data){
-        if(view == "overview"){
-            console.log("sadasd")
+
+function startRefreshTimer () {
+    refreshTimer = setInterval(function () {
+        loadScore("overview");
+    }, 5000)
+}
+
+function loadScore (view) {
+    $.post("getScore", function (data) {
+        if (view == "overview") {
             $(".scoreMain").html(scoreBoard(data));
+
+            clearInterval(refreshTimer);
+            startRefreshTimer();
+
+            // Mark score green if answer is given
+            $(".scoreNumber").each(function () {
+                var answerContent = $(this).data("content")
+
+                if (answerContent != "") {
+                    if (typeof (answerContent) !== "undefined") {
+                        $(this).addClass("hasAnswer");
+                    }
+                }
+            })
+
         } else if (view == "top") {
             var scoreViewBtn = Handlebars.compile($('#scoreViewBtn').html());
+
             // Append score view buttons for every round
             $(".scoreViewMain").append(scoreViewBtn(data.answers));
 
             var teamsArray = [];
             // Convert to array of objects
-            for(key in data.teams){
+            for (key in data.teams) {
                 var currTeam = data.teams[key];
                 currTeam["guid"] = key
                 teamsArray.push(currTeam);
             }
 
             // Sort top score
-            var sortedScore = teamsArray.sort(function(a, b){
+            var sortedScore = teamsArray.sort(function (a, b) {
                 return b.totalScore - a.totalScore
             })
             // Split first 3
             var scoreStage = [sortedScore[0], sortedScore[1], sortedScore[2]]
             var scoreOthers = sortedScore.splice(3, sortedScore.length);
 
-            $(".scoreMain").html(scoreBoard({ scoreStage: scoreStage, scoreOthers: scoreOthers}));
+            $(".scoreMain").html(scoreBoard({ scoreStage: scoreStage, scoreOthers: scoreOthers }));
         }
     })
 }
