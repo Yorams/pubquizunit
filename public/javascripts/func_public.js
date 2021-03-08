d5922b680692a2ec7e9d0025f8e6d955d32621e7
Yorams <yscheer@gmail.com>
2021-03-08 20:41:46 +0100

    Added a error message function for the user.

--- public/javascripts/func_public.js
+++ public/javascripts/func_public.js
@@ -1,3 +1,4 @@
+var errorTemplate = Handlebars.compile($('#errorTemplate').html());
 function sendPost (url, parameterA, parameterB) {
     if (typeof (parameterA) === "function") {
         callback = parameterA
@@ -9,9 +10,28 @@ function sendPost (url, parameterA, parameterB) {
 
     $.post(url, sendData, function (data) {
         if (data.result == "error" && data.errorCode == "logged_out") {
+            showError("You are logged out.", "You will be redirected to the login page")
             window.location.href = "/login"
         } else {
             callback(data);
         }
+    })
+}
+
+function showError (title, message) {
+
+    // Generate random id
+    var alertId = Math.floor(Math.random() * 10000000);
+
+    // Add alert to container
+    $(".errorContainer").append(errorTemplate({ title: title, message: message, id: alertId }))
+
+    // Set options and show
+    $(`.alert-${alertId}`).toast({ delay: 5000 })
+    $(`.alert-${alertId}`).toast("show");
+
+    // Remove element after it is done
+    $(`.alert-${alertId}`).on("hidden.bs.toast", function () {
+        $(`.alert-${alertId}`).remove();
     })
 }
