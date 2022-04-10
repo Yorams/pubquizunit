c80139b19533883a721cc44c64703e165df5d7fb
Yoram Scheer <yscheer@gmail.com>
2022-04-10 22:27:28 +0200

    changed init script

--- initialize_database.js
+++ initialize_database.js
@@ -19,8 +19,8 @@ module.exports = common.getJsonFile("/settings").then(function (appSettings) {
 })
     .catch((err) => {
         if (err.message.includes("ENOENT") && err.message.includes("settings")) {
-            log.error(`Cannot load settings file: settings.json. You have to rename settings_empty.json to settings.json and alter the settings in de file.`)
+            console.log(`Cannot load settings file: settings.json. You have to rename settings_empty.json to settings.json and alter the settings in de file.`)
         } else {
-            log.error(err.stack)
+            console.log(err.stack)
         }
     })
